e7099ae488de27040ef22321f221adc7e683471a
kate-pechinskaya <katttka98@gmail.com>
2021-04-17 12:00:50 +0300

    adds description for some programs

--- src/by/pev/unit2_Algorithmization/Decomposition14.java
+++ src/by/pev/unit2_Algorithmization/Decomposition14.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import static java.lang.Math.*;
+
 public class Decomposition14 {
     /*14.	Натуральное число, в записи которого n цифр, называется числом Армстронга,
      если сумма его цифр, возведенная в степень n, равна самому числу.
@@ -23,7 +25,8 @@ public class Decomposition14 {
                 j /= 10;
                 pow++;
             }
-            sum = (int) (Math.pow(number[0], pow) + Math.pow(number[1], pow) + Math.pow(number[2], pow) + Math.pow(number[3], pow) + Math.pow(number[4], pow) + Math.pow(number[5], pow));
+            sum = (int) (pow(number[0], pow) + pow(number[1], pow) + pow(number[2], pow)
+                    + pow(number[3], pow) + pow(number[4], pow) + pow(number[5], pow));
             if (sum == i)
                 System.out.print(i + "  ");
             pow = 0;
