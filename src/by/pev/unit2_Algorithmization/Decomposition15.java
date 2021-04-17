e52aa73a31c29e550c9786f391a9dbd63cb2fe07
kate-pechinskaya <katttka98@gmail.com>
2021-04-17 10:58:00 +0300

    correct some strings

--- src/by/pev/unit2_Algorithmization/Decomposition15.java
+++ src/by/pev/unit2_Algorithmization/Decomposition15.java
@@ -4,7 +4,20 @@ public class Decomposition15 {
     /*15.	Найти все натуральные n-значные числа, цифры в которых образуют строго возрастающую
      последовательность (например, 1234, 5789). Для решения задачи использовать
       декомпозицию. */
-    public static void main(String[] args) {
+    public static int enterN(int n){
+        int[]array = new int[10000];
+        for (int i = 0; i < 10000; i++) {
+            array[i] = i;
+            String lenght = Integer.toString(i);
+            if (lenght.length()==n)
+                for (int j = 0; i < n; j++) {//длинна
+//                    if()//скорей всего ещё фор для нахождения max(n) и сравнивания с мин
+                }
+        }
+        return array.length;
+    }
 
+    public static void main(String[] args) {
+        enterN(4);
     }
 }
