9c599bdfa91a0527197d5e00056e9fe0debb9513
kate-pechinskaya <katttka98@gmail.com>
2021-06-10 19:01:51 +0300

    added text base for unit5

---
+++ src/by/pev/unit5_OOP/Task2.java
@@ -0,0 +1,10 @@
+package by.pev.unit5_OOP;
+
+public class Task2 {
+    /*Задача 2.
+Создать класс Payment с внутренним классом, с помощью объектов которого можно сформировать покупку из нескольких товаров.
+ */
+    public static void main(String[] args) {
+
+    }
+}
